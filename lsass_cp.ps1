891e4440c8576cec49b7e80544f3204fdcc8cdca
NO53LF <30779753+UbuntuStrike@users.noreply.github.com>
2024-03-27 11:42:49 -0600

    Update lsass_cp.ps1

--- lsass_cp.ps1
+++ lsass_cp.ps1
@@ -1,16 +1,1 @@
-while ($true) {
-    $sourcePath = "$env:LOCALAPPDATA\Temp\lsass.DMP"
-    $destinationPath = "$env:USERPROFILE\Desktop\dumpedd_sass.DMP"
-    
-    # Check if lsass.DMP exists in the source path
-    if (Test-Path $sourcePath) {
-        # Copy lsass.DMP to destination path
-        Copy-Item -Path $sourcePath -Destination $destinationPath -Force
-        Write-Host "lsass.DMP copied to desktop"
-    } else {
-        Write-Host "lsass.DMP not found in Temp directory"
-    }
-    
-    # Pause for 0.25 seconds
-    Start-Sleep -Milliseconds 250
-}
+
